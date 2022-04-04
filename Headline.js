c01e0434f14110a6415c16072e6f4b3cbc13fb9c
jt4510 <jt4510@nyu.edu>
2022-04-04 12:04:55 -0400

    fancyheadline

--- Headline.js
+++ Headline.js
@@ -14,13 +14,12 @@
 const GWINDOW_WIDTH = 500;
 const GWINDOW_HEIGHT = 100;
 const TIME_STEP = 20;
-const DELTA_X = -2;
-const MESSAGE = "Coding with Javascript is cool";
+const DELTA_X = -4;
+// const MESSAGE = "Coding with Javascript is cool";
 const HEADLINE_FONT_FAMILY = "'Arial'";
 const HEADLINE_FONT_SIZE = "90";
 const ERROR_FONT_FAMILY = "'Arial'";
 const ERROR_FONT_SIZE = "15";
-
 
 /*
  * Defines the entire program.  Variables declared inside Headline
@@ -28,15 +27,21 @@ const ERROR_FONT_SIZE = "15";
  */
 
 function Headline() {
+    let headlines = "";
     let gw = GWindow(GWINDOW_WIDTH, GWINDOW_HEIGHT);
     let timer = undefined;
     let headlineMessage = GLabel();
+    let loadStatus = false;
+    getRSS();
 
-    if (validateInput()) {
-        headlineMessage = GLabel(MESSAGE.toUpperCase());
-        headlineMessage.setFont(HEADLINE_FONT_SIZE + "px " + HEADLINE_FONT_FAMILY);
-        gw.add(headlineMessage, gw.getWidth(), (gw.getHeight() + headlineMessage.getAscent()) / 2);
-        gw.addEventListener("click", startStopAnimation);
+    /* This function is called by getRSS after the headlines are loaded */
+    function initializeHeadline() {
+        if (validateInput()) {
+            headlineMessage = GLabel(headlines.toUpperCase());
+            headlineMessage.setFont(HEADLINE_FONT_SIZE + "px " + HEADLINE_FONT_FAMILY);
+            gw.add(headlineMessage, gw.getWidth(), (gw.getHeight() + headlineMessage.getAscent()) / 2);
+            gw.addEventListener("click", startStopAnimation);
+        }
     }
 
     /* This function is called each time the interval timer ticks to update the animation */
@@ -66,13 +71,13 @@ function Headline() {
      * If not, it displays an appropriate message in the graphics window
      */
     function validateInput() {
-        if (MESSAGE.length >= 20 && MESSAGE.length <= 30) {
+        if (headlines.length >= 20 && headlines.length <= 30000) {
             return true;
-        } else if (MESSAGE.length < 20) {
-            reportError("short");
+        } else if (headlines.length < 20) {
+            reportStatus("too short");
             return false;
-        } else if (MESSAGE.length > 30) {
-            reportError("long");
+        } else if (headlines.length > 30000) {
+            reportStatus("too long");
             return false;
         }
     }
@@ -80,10 +85,37 @@ function Headline() {
     /*
      * This function is called by validateInput() and will display the error that is detected.
      */
-    function reportError(type) {
+    function reportStatus(type) {
         let error = "Message is too " + type + ".";
         let errorMessage = GLabel(error.toUpperCase());
         errorMessage.setFont(ERROR_FONT_SIZE + "px " + ERROR_FONT_FAMILY);
         gw.add(errorMessage, (gw.getWidth() - errorMessage.getWidth()) / 2, (gw.getHeight() + errorMessage.getAscent()) / 2);
+    }
+    /*
+     * This function is called by validateInput() and will display the error that is detected.
+     */
+    function getRSS() {
+        fetch("https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml")
+            .then(response => {
+                if(!response.ok) {
+                    console.log("It didn't work");
+                }
+                return response.text();
+            })
+            .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+            .then(data => {
+                // console.log(data);
+                const items = data.querySelectorAll("item");
+                items.forEach(el => {
+                    let thisHeadline = el.querySelector("title").innerHTML;
+                    console.log(thisHeadline);
+                    console.log(thisHeadline.length);
+                    headlines += thisHeadline;
+                    headlines += " ... ";
+                });
+                initializeHeadline();
+                // console.log(headlines.toUpperCase());
+                // console.log(headlines.length);
+            });
     }
 }
